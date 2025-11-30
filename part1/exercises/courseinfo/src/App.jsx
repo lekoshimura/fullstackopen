0e8a27cd1f1886bd9d5f255f0d6e81d7282765c1
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2025-11-30 19:40:29 -0300

    ex 1.3: Course Information step 3

--- part1/exercises/courseinfo/src/App.jsx
+++ part1/exercises/courseinfo/src/App.jsx
@@ -30,11 +30,19 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const courses = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+  const courses = [part1, part2, part3];
 
   return (
     <div>
