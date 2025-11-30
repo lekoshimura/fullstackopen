22097ef9553c94e0c8fa90e8ebc05a79ce337e85
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2025-11-30 19:47:05 -0300

    ex 1.4: Course Information step 4

---
+++ part1/ex-1.4-course-information-step-4.md
@@ -0,0 +1,43 @@
+# 1.4: Course Information step 4
+
+Place the objects into an array. Modify the variable definitions of App into the following form and modify the other parts of the application accordingly:
+
+```jsx
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  return <div>...</div>;
+};
+```
+
+NB at this point you can assume that there are always three items, so there is no need to go through the arrays using loops. We will come back to the topic of rendering components based on items in arrays with a more thorough exploration in the next part of the course.
+
+However, do not pass different objects as separate props from the App component to the components Content and Total. Instead, pass them directly as an array:
+
+```jsx
+const App = () => {
+  // const definitions
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+```
--- part1/exercises/courseinfo/src/App.jsx
+++ part1/exercises/courseinfo/src/App.jsx
@@ -3,52 +3,55 @@ const Header = (props) => {
 };
 
 const Part = (props) => {
+  console.log('Part', props);
   return (
     <p>
-      {props.course.name}: {props.course.exercises} exercises
+      {props.name}: {props.exercises} exercises
     </p>
   );
 };
 
 const Content = (props) => {
+  console.log('Content', props.parts);
   return (
     <>
-      <Part course={props.courses[0]} />
-      <Part course={props.courses[1]} />
-      <Part course={props.courses[2]} />
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   );
 };
 
 const Total = (props) => {
-  let sum = 0;
-  for (let i = 0; i < props.courses.length; i++) {
-    sum += props.courses[i].exercises;
-  }
+  let sum =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
   return <p>Number of exercises: {sum}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-  const courses = [part1, part2, part3];
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content courses={courses} />
-      <Total courses={courses} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
