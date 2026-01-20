9057811b08d90714fb80c551ed4c8b96f52d1a6f
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2026-01-20 07:21:57 -0300

    fix: ex 2.1 application must work regardless of the number of parts a course has

--- part2/exercises/courseinfo/src/Course.jsx
+++ part2/exercises/courseinfo/src/Course.jsx
@@ -13,21 +13,17 @@ const Part = (props) => {
 
 const Content = (props) => {
   console.log("Content", props.parts);
-  return (
-    <>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
-  );
+  return props.parts.map((part) => (
+    <Part key={part.id} name={part.name} exercises={part.exercises} />
+  ));
 };
 
 const Course = ({ course }) => {
   return (
-    <div>
+    <>
       <Header course={course.name} />
       <Content parts={course.parts} />
-    </div>
+    </>
   );
 };
 
